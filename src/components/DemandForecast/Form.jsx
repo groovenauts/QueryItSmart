90cb4a5d5babd5fb09f277bc14286d0906f03302
nakanori <n-nakamura@groovenauts.jp>
2017-02-28 10:19:46 +0900

    Make it customize the select-box

--- src/components/DemandForecast/Form.jsx
+++ src/components/DemandForecast/Form.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import classNames from 'classnames'
 
-import { deepPurple900 } from 'material-ui/styles/colors'
+import { indigo900, grey400 } from 'material-ui/styles/colors'
 import { bindActionCreators } from 'redux'
 import * as actions from '../../actions/forecastActions'
 import { connect } from 'react-redux'
@@ -36,7 +36,7 @@ class Form extends Component {
                 left: 0,
                 top: 0,
                 bottom: 0,
-                color: deepPurple900,
+                color: indigo900,
                 wordWrap: 'breakWord',
                 maxWidth: '80%',
                 transform: 'translateY(14vh)',
@@ -54,17 +54,18 @@ class Form extends Component {
     )
   }
 
-  renderSelectField(name, label, value, values) {
+  renderSelectField(name, label, value, values, placeholder) {
     let options = _.map(values, (text, i) => {
       return (
-        <option key={`weekday-${i}`} value={i}>
+        <option key={`${name}-${i+1}`} value={i}>
           {text}
         </option>
       )
     })
-    // options.shift(<option value="" disabled selected hidden>Please Choose...</option>)
+    options.unshift(<option key={`${name}-0`} value="" disabled hidden>{ placeholder }</option>)
     return (
       <select
+        style={{color: `${_.isEmpty(value) ? grey400:indigo900}`}}
         name={name}
         placeholder={label}
         value={value}
@@ -76,10 +77,20 @@ class Form extends Component {
 
   renderContent() {
     const { param } = this.props.forecast
+    const disabled = _.reduce(param, (ret, value, key) => {
+      if (!ret && _.isEmpty(value)) {
+        ret = true
+      }
+      return ret
+    }, false)
     return (
       <div id="demand-forecast-content" style={{zIndex:2}}>
         <div className="row center-xs">
-          <div className="col-xs-8" style={{backgroundColor: 'white'}}>
+          <div className="col-xs-8" style={{
+              backgroundColor: 'white',
+              paddingLeft: 40,
+              paddingRight: 40,
+            }}>
             <div className="box"
               style={{
                 fontSize: 44,
@@ -95,25 +106,25 @@ class Form extends Component {
                 padding: 10,
               }}>
               <div className="row center-xs">
-                <div className="col-xs-2">
-                  <div className="box">
-                    { this.renderSelectField("weekday", "WEEKDAYS", param.weekday, WEEKDAYS) }
-                  </div>
+                <div className="col-xs-3">
+                  <section className="box select-box">
+                    { this.renderSelectField("month", "MONTHS", param.month, MONTHS, lang.demandForecast.form.month) }
+                  </section>
                 </div>
-                <div className="col-xs-2">
-                  <div className="box">
-                    { this.renderSelectField("month", "MONTHS", param.month, MONTHS) }
-                  </div>
+                <div className="col-xs-3">
+                  <section className="box select-box">
+                    { this.renderSelectField("weekday", "WEEKDAYS", param.weekday, WEEKDAYS, lang.demandForecast.form.wday) }
+                  </section>
                 </div>
-                <div className="col-xs-2">
-                  <div className="box">
-                    { this.renderSelectField("weather", "WEATHERS", param.weather, WEATHERS) }
-                  </div>
+                <div className="col-xs-3">
+                  <section className="box select-box">
+                    { this.renderSelectField("weather", "WEATHERS", param.weather, WEATHERS, lang.demandForecast.form.weather) }
+                  </section>
                 </div>
-                <div className="col-xs-2">
-                  <div className="box">
-                    { this.renderSelectField("temperature", "TEMPERATURES", param.temperature, TEMPERATURES) }
-                  </div>
+                <div className="col-xs-3">
+                  <section className="box select-box">
+                    { this.renderSelectField("temperature", "TEMPERATURES", param.temperature, TEMPERATURES, lang.demandForecast.form.temperature) }
+                  </section>
                 </div>
               </div>
             </div>
@@ -123,16 +134,18 @@ class Form extends Component {
                 paddingTop: 100,
                 paddingBottom: 100,
                 fontSize: 30,
+                opacity: `${disabled ? "0.2":"1"}`,
               }}>
               <Circle
                 onClick={this.onSubmit.bind(this)}
-                innerClassName="citibike-start-button"
+                innerClassName={ classNames("citibike-start-button", `${disabled ? "disabled":""}`) }
                 style={{
                   position: 'absolute',
                   height: 100,
                   width: 100,
                   opacity: 1,
                   lineHeight: '100px',
+                  pointerEvents: `${disabled ? "none":"auto"}`,
                 }}>
                 Start
               </Circle>
