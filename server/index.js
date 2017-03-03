03bb91eb509cdf8db7c03c5e34fddfd9ad17825c
nakanori <n-nakamura@groovenauts.jp>
2017-03-04 08:40:11 +0900

    Make it passing api key with node env

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('template', {
+    apiKey: process.env.API_KEY
+  });
 });
 
 module.exports = router;
