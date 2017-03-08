4a2cbcf8de04c600d36f53478b1bb636c418d8ac
nakanori <n-nakamura@groovenauts.jp>
2017-03-08 10:56:59 +0900

    Make it passing image base url with 'IMAGE_BASE_URL' enviroment variable

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   res.render('template', {
-    apiKey: process.env.API_KEY
+    apiKey: process.env.API_KEY,
+    imageBaseUrl: process.env.IMAGE_BASE_URL,
   });
 });
 
