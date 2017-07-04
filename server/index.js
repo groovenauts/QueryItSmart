6bc83ee095b089aaba8d0a0960548123eb42d9b1
nakanori <n-nakamura@groovenauts.jp>
2017-07-04 16:45:24 +0900

    Add environment variable for google analytics

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const API_KEY = process.env.API_KEY;
 const IMAGE_BASE_URL = process.env.IMAGE_BASE_URL;
 const SITE_KEY = process.env.RECAPTCHA_SITE_KEY;
 const SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY;
+const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
 
 const hasAuthentication = SITE_KEY && SECRET_KEY
 if (hasAuthentication) {
@@ -15,6 +16,7 @@ if (hasAuthentication) {
 const templateOption = {
   apiKey: API_KEY,
   imageBaseUrl: IMAGE_BASE_URL,
+  gaTrackingId: GA_TRACKING_ID,
 };
 const loginOption = {
   siteKey: SITE_KEY,
