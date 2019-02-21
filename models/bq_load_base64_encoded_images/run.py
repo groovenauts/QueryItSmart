08f6809dfa6aefd6b888cf0653fc6cf91d9e433e
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2019-02-21 14:44:06 +0900

    Add Cloud Dataflow pipeline for load base64 encoded images to BigQuery.

---
+++ models/bq_load_base64_encoded_images/run.py
@@ -0,0 +1,48 @@
+import argparse
+import json
+import os
+import base64
+import logging
+
+import apache_beam as beam
+from google.cloud import storage as gcs
+
+class Encode64DoFn(beam.DoFn):
+    def process(self, element, bucket_name, prefix):
+        key = element["key"]
+        name = prefix + "/" + key + ".jpg"
+        result = {}
+        result["key"] = key
+        client = gcs.Client()
+        bucket = client.get_bucket(bucket_name)
+        blob = gcs.Blob(name, bucket)
+        result["image_base64"] = base64.urlsafe_b64encode(blob.download_as_string())
+        return [result]
+
+def run(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-table", dest="input_table", required=True,
+            help='Input BigQuery table name.')
+    parser.add_argument("--output-table", dest="output_table", required=True,
+            help="Output BigQuery table name.")
+    parser.add_argument("--image-bucket", dest="image_bucket", required=True,
+            help="Bucket name contains images.")
+    parser.add_argument("--image-prefix", dest="image_prefix", required=True,
+            help="GCS object name prefix.")
+    known_args, pipeline_args = parser.parse_knownargs(argv)
+
+    p = beam.Pipeline(argv.pipeline_args)
+    schema = 'key:STRING, image_base64:STRING'
+    images = p | 'ReadFromBQ' >> beam.io.Read(beam.io.BigQuerySource(known_args.input_table))
+    b64encode = images | 'Encode64' >> beam.ParDo(Encode64DoFn(), known_args.image_bucket, known_args.image_prefix)
+    b64encode | 'OutputToBQ' >> beam.io.Write(beam.io.BigQuerySink(
+        known_args.output_table,
+        schema=schema,
+        create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+        write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE))
+
+    p.run()
+
+if __name__== '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
