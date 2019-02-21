64d25b5cfd42d04b835a2793f64a0715c97a5961
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2019-02-21 17:18:02 +0900

    Add command line option --project_id to pass Poject ID to gcs.Client()

--- models/bq_load_base64_encoded_images/run.py
+++ models/bq_load_base64_encoded_images/run.py
@@ -5,14 +5,14 @@ import logging
 import apache_beam as beam
 
 class Encode64DoFn(beam.DoFn):
-    def process(self, element, bucket_name, prefix):
+    def process(self, element, project_id, bucket_name, prefix):
         import base64
         from google.cloud import storage as gcs
         key = element["key"]
         name = prefix + "/" + key + ".jpg"
         result = {}
         result["key"] = key
-        client = gcs.Client()
+        client = gcs.Client(project_id)
         bucket = client.get_bucket(bucket_name)
         blob = gcs.Blob(name, bucket)
         result["image_base64"] = base64.urlsafe_b64encode(blob.download_as_string())
@@ -20,6 +20,8 @@ class Encode64DoFn(beam.DoFn):
 
 def run(argv=None):
     parser = argparse.ArgumentParser()
+    parser.add_argument("--project-id", dest="project_id", required=True,
+            help='GCP Project ID.')
     parser.add_argument("--input-table", dest="input_table", required=True,
             help='Input BigQuery table name.')
     parser.add_argument("--output-table", dest="output_table", required=True,
@@ -33,7 +35,7 @@ def run(argv=None):
     p = beam.Pipeline(argv=pipeline_args)
     schema = 'key:STRING, image_base64:STRING'
     images = p | 'ReadFromBQ' >> beam.io.Read(beam.io.BigQuerySource(known_args.input_table))
-    b64encode = images | 'Encode64' >> beam.ParDo(Encode64DoFn(), known_args.image_bucket, known_args.image_prefix)
+    b64encode = images | 'Encode64' >> beam.ParDo(Encode64DoFn(), known_args.project_id, known_args.image_bucket, known_args.image_prefix)
     b64encode | 'OutputToBQ' >> beam.io.Write(beam.io.BigQuerySink(
         known_args.output_table,
         schema=schema,
