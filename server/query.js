0cb9c3c5d100a2fb07dd4c8d56e7824206cf090f
anupamme <mediratta@gmail.com>
2026-09-05 12:13:55 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- server/query.js
+++ server/query.js
@@ -4,9 +4,14 @@ var _ = require("lodash");
 var QUERY_TEMPLATES = require("../data/queries.json");
 
 router.post('/', function(req, res) {
-  sql = _.template(QUERY_TEMPLATES[req.body.queryType])(req.body.queryParams)
+  if (!_.has(QUERY_TEMPLATES, req.body.queryType)) {
+    return res.status(400).send('Invalid query type');
+  }
+  // Use BigQuery named query parameters instead of interpolating
+  // user-supplied values into the SQL text, preventing SQL injection.
   options = req.body.options
-  options.query = sql
+  options.query = QUERY_TEMPLATES[req.body.queryType]
+  options.params = req.body.queryParams
   bigQuery.query(options)
     .then((results) => {
       res.status(200).send(results);
